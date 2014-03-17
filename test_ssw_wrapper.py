939f18c96591c724d0fb65a3dc8c4e1694922d7a
= <ebolyen@gmail.com>
2014-03-17 17:26:18 -0600

    tests to follow

--- test_ssw_wrapper.py
+++ test_ssw_wrapper.py
@@ -7,3 +7,22 @@
 #
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
+
+from unittest import TestCase, main
+from ssw import StripedSmithWaterman, striped_smith_waterman_alignment
+
+class TestSSW(TestCase):
+    def test_many_targets(self):
+        pass
+
+    def test_uppercase(self):
+        pass
+
+    def test_lowercase(self):
+        pass
+
+    def test_striped_smith_waterman_alignment(self):
+        pass
+
+if __name__ == '__main__':
+    main()
